bc492b4d8882083dd0cd1be04280fc0cbd47069d
Luís <luis.rabock@hotmail.com>
2018-02-26 14:22:59 -0300

    Add files via upload

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,27 +1,29 @@
 module.exports.cadastro = function(application, req, res){
-    res.render('cadastro', { validacao: {}, dadosForm: {} });
+    res.render('cadastro', { validacao: {}, dadosForm: {} })
 }
 
 module.exports.cadastrar = function(application, req, res){
 
     const dadosForm = req.body;
 
-    req.assert('nome', 'Nome não pode ser vazio').notEmpty();
-    req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
-    req.assert('senha', 'Senha não pode ser vazia').notEmpty();
-    req.assert('casa', 'Casa não pode ser vazia').notEmpty();
+    req.assert('nome', 'Nome não pode ser vazio').notEmpty()
+    req.assert('usuario', 'Usuário não pode ser vazio').notEmpty()
+    req.assert('senha', 'Senha não pode ser vazia').notEmpty()
+    req.assert('casa', 'Casa não pode ser vazia').notEmpty()
 
     const erros = req.validationErrors();
 
     if(erros){
-        res.render('cadastro', { validacao: erros, dadosForm: dadosForm });
+        res.render('cadastro', { validacao: erros, dadosForm: dadosForm })
         return;
     }
 
-    const connection = application.config.dbConnection;
-    const UsuariosDAO = new application.app.models.UsuariosDAO(connection);
+    const connection = application.config.dbConnection
+    const UsuariosDAO = new application.app.models.UsuariosDAO(connection)
+    const JogoDAO = new application.app.models.JogoDAO(connection)
 
-    UsuariosDAO.inserirUsuario(dadosForm);
+    UsuariosDAO.inserirUsuario(dadosForm)
+    JogoDAO.gerarParametros(dadosForm.usuario)
 
     res.send('podemos cadastrar');
 
--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,15 +1,91 @@
-module.exports.jogo = (application,req,res) => {
-    if(req.session.autorizado) {
-        res.render('jogo',{img_casa: req.session.casa})
-    } else {
-        res.send('Usuario precisa estar logado na aplicação')
+module.exports.jogo = function(application, req, res){
+
+    if(req.session.autorizado !== true){
+        res.send('Usuário precisa fazer login');
+        return;
     }
+
+    var msg = '';
+    if(req.query.msg != ''){
+        msg = req.query.msg;
+    }
+
+    var usuario = req.session.usuario;
+    var casa = req.session.casa;
+
+    var connection = application.config.dbConnection;
+    var JogoDAO = new application.app.models.JogoDAO(connection);
+
+    JogoDAO.iniciaJogo(res, usuario, casa, msg);
 }
 
-module.exports.sair = (application,req,res) => {
+module.exports.sair = function(application, req, res){
+    req.session.destroy(function(err){
+        res.render('index', { validacao: {} });
+    });
+}
 
-    req.session.destroy( function(err) {
-        res.render('index', {validacao:{}})
-    })
+module.exports.suditos = function(application, req, res){
+    if(req.session.autorizado !== true){
+        res.send('Usuário precisa fazer login');
+        return;
+    }
 
+    res.render('aldeoes', { validacao: {} });
+}
+
+module.exports.pergaminhos = function(application, req, res){
+    if(req.session.autorizado !== true){
+        res.send('Usuário precisa fazer login');
+        return;
+    }
+    console.log('entrou')
+    var connection = application.config.dbConnection
+    var JogoDAO = new application.app.models.JogoDAO(connection)
+
+    var usuario = req.session.usuario
+
+    JogoDAO.getAcoes(usuario, res)
+
+
+}
+
+
+
+module.exports.ordenar_acao_sudito = function(application, req, res){
+    if(req.session.autorizado !== true){
+        res.send('Usuário precisa fazer login');
+        return;
+    }
+
+    var dadosForm = req.body;
+
+    req.assert('acao', 'Ação deve ser informada').notEmpty();
+    req.assert('quantidade', 'Quantidade deve ser informada').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.redirect('jogo?msg=A');
+        return;
+    }
+
+    var connection = application.config.dbConnection;
+    var JogoDAO = new application.app.models.JogoDAO(connection);
+
+    dadosForm.usuario = req.session.usuario;
+    JogoDAO.acao(dadosForm);
+
+    res.redirect('jogo?msg=B');
+
+}
+
+module.exports.revogar_acao = function(application, req, res){
+    var url_query = req.query
+    var connection = application.config.dbConnection;
+    var JogoDAO = new application.app.models.JogoDAO(connection);
+
+    var _id = url_query.id_acao
+    console.log(_id)
+    JogoDAO.revogarAcao(_id)
 }
